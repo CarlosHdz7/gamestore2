768d62232542c8690f29d09612ce139f42afd2e7
Carlos Eduardo Cabrera Hernández <carloshdz.udb@gmail.com>
2021-07-21 19:14:10 -0600

    ⚡️ Add useCallback to function

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,36 +1,42 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import PropTypes from 'prop-types';
 import './Home.scss';
 
-const Home = ({ setPage }) => (
-  <>
-    <div className="home-container">
-      <div className="container-info">
-        <h4 className="container-info__title">Welcome to our store</h4>
-        <p className="container-info__subtitle">
-          Where you can find a lot of new titles of your favorites consoles
-          everyday at good price.
-        </p>
-        <div className="button-container-home">
-          <button
-            type="button"
-            className="container-info__button"
-            onClick={() => setPage('list')}
-          >
-            Go to store
-          </button>
+const Home = ({ setPage }) => {
+  const handleClick = useCallback(() => {
+    setPage('list');
+  }, []);
+
+  return (
+    <>
+      <div className="home-container">
+        <div className="container-info">
+          <h4 className="container-info__title">Welcome to our store</h4>
+          <p className="container-info__subtitle">
+            Where you can find a lot of new titles of your favorites consoles
+            everyday at good price.
+          </p>
+          <div className="button-container-home">
+            <button
+              type="button"
+              className="container-info__button"
+              onClick={handleClick}
+            >
+              Go to store
+            </button>
+          </div>
+        </div>
+        <div className="container-image">
+          <img
+            className="container-image__img"
+            src="/images/controller.png"
+            alt="img-homepage"
+          />
         </div>
       </div>
-      <div className="container-image">
-        <img
-          className="container-image__img"
-          src="/images/controller.png"
-          alt="img-homepage"
-        />
-      </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
 
 Home.propTypes = {
   setPage: PropTypes.func.isRequired,
