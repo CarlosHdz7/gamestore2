9a500e04917706741ee0b1aba7e7379bf7b025bb
Carlos Eduardo Cabrera Hernández <carloshdz.udb@gmail.com>
2021-07-24 08:53:50 -0600

    🔥 Delete unnecessary code

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,9 +1,8 @@
-import { useState, useCallback } from 'react';
+import { useCallback } from 'react';
 import useLocalStorage from './useLocalStorage';
 
 const useAuth = () => {
   const [storage, setStorage] = useLocalStorage('user');
-  const [, setIsLogged] = useState(false);
 
   const isAuthenticated = useCallback(
     () => {
@@ -16,12 +15,10 @@ const useAuth = () => {
 
   const login = (data) => {
     setStorage(data);
-    setIsLogged(true);
   };
 
   const logout = () => {
     setStorage({});
-    setIsLogged(false);
   };
 
   return { isAuthenticated, login, logout };
